fe26ee537b536666626b4a3a5574133e884b8f50
John Doe <balint.portella@gmail.com>
2022-02-28 12:05:31 +0100

    profil jelszomegerősítéssel

--- oldalak/profil_szerkesztese/profil_szerkesztese_tartalom.php
+++ oldalak/profil_szerkesztese/profil_szerkesztese_tartalom.php
@@ -1,7 +1,7 @@
 <div class="d-flex">
     <div class="container ">
         <div class="welcome-massege mx-auto border border-success">
-                <h3>Profil</h3>
+                <h3> <?php echo $row['felhasznalonev']?> profilja</h3>
         </div>
     </div>
 </div><br>
@@ -14,13 +14,15 @@
   <?php echo $errors;
         echo $info; ?>
     
-  
+   
   <div class="row">
+  
     <div class="d-flex justify-content-center">
       <div class ="col-12 col-md-9 col-lg-7">
-        <div class="form-floating mb-3 mt-3">
+        <div class="mb-3 mt-3">
+        <label class="float-start" for="felhasznalonev">Felhasználónév frissítése:</label>
             <input type="text" class="form-control" id="felhasznalonev" placeholder="Felhasználónév" value="<?php echo $row['felhasznalonev']; ?>" name="felhasznalonev" >
-            <label for="felhasznalonev">Felhasználónév</label>
+           
         </div>
       </div>
     </div>
@@ -29,9 +31,22 @@
   <div class="row">
     <div class="d-flex justify-content-center">
       <div class ="col-12 col-md-9 col-lg-7">
-        <div class="form-floating mb-3 mt-3">
-            <input type="password" class="form-control" id="jelszo" placeholder="Jelszó" name="jelszo" >
-            <label for="jelszo">Jelszó</label>
+        <div class="mb-3 mt-3">
+        <label class="float-start" for="password_now">Jelenlegi jelszó:</label>
+            <input type="password" class="form-control" id="jelszo_uj" placeholder="Jelenlegi jelszó" name="jelszo_most" >
+            
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="d-flex justify-content-center">
+      <div class ="col-12 col-md-9 col-lg-7">
+        <div class="mb-3 mt-3">
+        <label class="float-start" for="password">Új jelszó:</label>
+            <input type="password" class="form-control" id="jelszo_uj" placeholder="Új jelszó" name="jelszo_uj" >
+            
         </div>
       </div>
     </div>
--- oldalak/profil_szerkesztese/profil_szerkesztese_vezerlo.php
+++ oldalak/profil_szerkesztese/profil_szerkesztese_vezerlo.php
@@ -2,6 +2,7 @@
 $errors = "";
 $info="";
 
+
 $id=$user->GetId();
 $query=mysqli_query($kapcs->db,"SELECT * FROM felhasznalok where id='$id'")or die(mysqli_error());
 $row=mysqli_fetch_array($query);
@@ -9,10 +10,19 @@ $row=mysqli_fetch_array($query);
   
    if(isset($_POST['profil_update'])) {
     $uj_felhasznalonev = mysqli_real_escape_string($kapcs->db, $_POST['felhasznalonev']);
-    $uj_jelszo = mysqli_real_escape_string($kapcs->db, $_POST['jelszo']);
-        
+    $uj_jelszo = mysqli_real_escape_string($kapcs->db, $_POST['jelszo_uj']);
+    $jelnlegi_jelszo=mysqli_real_escape_string($kapcs->db,$_POST['jelszo_most']);
+    $jelenlegi_felhasznalonev = $row['felhasznalonev'];
         $uj_jelszo = md5($uj_jelszo);
+        $jelnlegi_jelszo=md5($jelnlegi_jelszo);
+
+        $sql = "SELECT jelszo FROM felhasznalok WHERE felhasznalonev=$jelenlegi_felhasznalonev";
+        $egyezo_jelszo_lekerdezes=$kapcs->Select($sql);
+        print_r($egyezo_jelszo_lekerdezes['jelszo']);
+        
+        if ($egyezo_jelszo_lekerdezes['jelszo'] == $jelnlegi_jelszo){
         $user_id=$user->GetId();
+        if ($jelenlegi)
         $query = "UPDATE felhasznalok SET jelszo='$uj_jelszo',felhasznalonev='$uj_felhasznalonev' where id='$user_id'";
         
         $result = mysqli_query($kapcs->db, $query) or die(mysqli_error($kapcs->db));
@@ -23,11 +33,13 @@ $row=mysqli_fetch_array($query);
     </script>
     <?php 
         $user->Login($id,$uj_felhasznalonev,$row['email'],$row['jogosultsag']);
-        header("Refresh:0");
+        header("Refresh:0");}else {$errors="Helytelen jelszó!";}
+        } 
+
         
    
         
     
-}
+
 
 ?>
